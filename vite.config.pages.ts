4843cce0f4eb2d372d351c5acaa11357465b4155
latorocka <59487144+latorocka@users.noreply.github.com>
2026-04-30 10:38:58 -0700

    Change base path in Vite config to root

--- vite.config.pages.ts
+++ vite.config.pages.ts
@@ -4,7 +4,7 @@ import path from "path";
 
 export default defineConfig({
   plugins: [react()],
-  base: '/GutShotKitchenWebsite/',
+  base: '/',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "client", "src"),
